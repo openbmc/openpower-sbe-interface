a63e9cf38031583b1f743fc23a18e66a0a1e19c8
Murulidhar Nataraju <murulidhar@in.ibm.com>
2017-03-27 05:58:47 -0500

    Interface definition for getScom and putScom operations
    These interfaces will be used for reading and writing to the SCOM
    registers. Details of exception handling is not included in this
    commit.
    
    Change-Id: I407f446c8de137b7ffc0ce83e837f00c6271ee85
    Signed-off-by: Murulidhar Nataraju <murulidhar@in.ibm.com>

---
+++ sbe_interfaces.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+namespace openpower
+{
+namespace sbe
+{
+
+/**
+ * @brief Reads a SCOM register in processor chip.
+ *
+ * Throws an exception on error.
+ *
+ * @param[in] FIFO device path associated with the SBE.
+ * @param[in] SCOM register address.
+ * @return The register data
+ */
+uint64_t getScom(const std::string& devPath,
+                 uint64_t address);
+
+
+/**
+ * @brief Writes to a SCOM register in processor chip.
+ *
+ * Throws an exception on error.
+ *
+ * @param[in] FIFO device path associated with the SBE.
+ * @param[in] SCOM register address.
+ * @param[in] Data to be written into the register.
+ */
+void putScom(const std::string& devPath,
+             uint64_t address,
+             uint64_t data);
+
+}//namespace sbe
+}//namespace openpower
