66c69797f30a0f7b680b0f076be749572b68ae06
Murulidhar Nataraju <murulidhar@in.ibm.com>
2017-04-20 12:05:51 -0500

    Implement parsing of SBE response data
    Changes include:
    - Utility interface for parsing the response from the SBE.
    
    Change-Id: Ib05d40a7b9257f0cd53597e1fdee5677994072b1
    Signed-off-by: Murulidhar Nataraju <murulidhar@in.ibm.com>

---
+++ sbe_chipOp_handler.cpp
@@ -0,0 +1,114 @@
+#include <stdexcept>
+#include <array>
+#include <sbe_chipOp_handler.hpp>
+namespace openpower
+{
+namespace sbe
+{
+namespace fifoutils
+{
+
+/*! \union sbeRespHeader_t
+ *  Defines the breakup of the SBE FIFO chip operation response header.
+ */
+union sbeRespHeader_t
+{
+    uint32_t commandWord; /**<Entire 32 bit command word */
+    struct
+    {
+        uint8_t command: 8; /**<command value for which response is obtained */
+        uint8_t commandClass: 8; /**< Class of the command */
+        uint16_t magic: 16; /**< Magic code obtained in the response */
+    };
+} __attribute__((packed));
+
+
+static constexpr uint16_t MAGIC_CODE = 0xC0DE;
+static constexpr auto SBE_OPERATION_SUCCESSFUL = 0;
+static constexpr auto LENGTH_OF_DISTANCE_HEADER_IN_WORDS = 0x1;
+
+#define SBEI_SBE_RESPONSE_SIZE_IN_WORDS  ( (sizeof(sbeRespHeader_t) + \
+        sizeof(sbe_word_t)) / 4 )
+
+
+std::vector<sbe_word_t> write(const char* devPath,
+                              const sbe_word_t* cmdBuffer,
+                              const size_t cmdBufLen,
+                              const size_t respBufLen)
+{
+    //TODO: Add support for reading and writing from the FIFO device
+    //Until then just return hardcoded data.
+    std::vector<sbe_word_t> response(respBufLen);
+    response = {0x80200000, 0x00000000, 0xc0dea201, 0x00000000, 0x00000003};
+    return (response);
+}
+
+std::vector<sbe_word_t> parseResponse(
+    const std::vector<sbe_word_t>& sbeDataBuf)
+{
+
+    //Number of 32-bit words obtained from the SBE
+    size_t lengthObtained = sbeDataBuf.size();
+
+    //Fetch the SBE header and SBE chiop primary and secondary status
+    //Last value in the buffer will have the offset for the SBE header
+    size_t DistanceToStatusHeader =  sbeDataBuf[sbeDataBuf.size() - 1];
+
+    if (lengthObtained < DistanceToStatusHeader)
+    {
+        //TODO:use elog infrastructure
+        std::ostringstream errMsg;
+        errMsg << "Distance to SBE status header value " <<
+               DistanceToStatusHeader << " is greater then total lenght of "
+               "response buffer " << lengthObtained;
+        throw std::runtime_error(errMsg.str().c_str());
+    }
+
+    //Fetch the response header contents
+    sbeRespHeader_t l_respHeader;
+    auto iter = sbeDataBuf.begin();
+    std::advance(iter, (lengthObtained - DistanceToStatusHeader));
+    l_respHeader.commandWord = *iter;
+
+    //Fetch the primary and secondary response code
+    std::advance(iter, 1);
+    sbe_word_t l_priSecResp = *iter;
+
+    //Validate the magic code obtained in the response
+    if (l_respHeader.magic != MAGIC_CODE)
+    {
+        //TODO:use elog infrastructure
+        std::ostringstream errMsg;
+        errMsg << "Invalid MAGIC keyword in the response header (" <<
+               l_respHeader.magic << "),expected keyword " << MAGIC_CODE;
+        throw std::runtime_error(errMsg.str().c_str());
+    }
+
+    //Validate the Primary and Secondary response value
+    if (l_priSecResp != SBE_OPERATION_SUCCESSFUL)
+    {
+        //Extract the SBE FFDC and throw it to the caller
+        size_t ffdcLen = (DistanceToStatusHeader -
+                          SBEI_SBE_RESPONSE_SIZE_IN_WORDS -
+                          LENGTH_OF_DISTANCE_HEADER_IN_WORDS);
+
+        std::vector<sbe_word_t> ffdcData(ffdcLen);
+        std::copy_n((sbeDataBuf.begin() + SBEI_SBE_RESPONSE_SIZE_IN_WORDS),
+                    ffdcLen, ffdcData.begin());
+
+        //TODO:use elog infrastructure to return the SBE and Hardware procedure
+        //FFDC container back to the caller.
+        std::ostringstream errMsg;
+        errMsg << "Chip operation failed,FFDC data of length:" << ffdcLen <<
+               " obtained!";
+        throw std::runtime_error(errMsg.str().c_str());
+    }
+    return std::move(sbeDataBuf);
+
+}
+
+}
+}
+}
+
+
---
+++ sbe_chipOp_handler.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <stdexcept>
+#include <array>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+namespace openpower
+{
+namespace sbe
+{
+
+using sbe_word_t = uint32_t;
+
+namespace fifoutils
+{
+/**
+ * @brief Helper function for invokeSBEChipOperation(),to write to the SBE FIFO
+ * device and obtain the expected response .Internal device driver failures
+ * will be conveyed via respective exceptions.
+ *
+ * Exceptions thrown for:
+ * - Device driver internal failures
+ *
+ * @param[in] FIFO device path associated with SBE.
+ * @param[in] Command buffer to be written to the SBE FIFO
+ * @param[in] Length of command buffer
+ * @param[in] Expected response buffer length
+ *
+ * @return Response buffer returned by the SBE for the input command.
+ */
+std::vector<sbe_word_t>  write(const char* devPath,
+                               const sbe_word_t* cmdBuffer,
+                               const size_t cmdBufLen,
+                               const size_t respBufLen);
+
+/**
+ * @brief Helper function for invokeSBEChipOperation(), to parse the data
+ * obtained from the SBE. The header and SBE response will be verified and on
+ * success the required data will be returned to the caller. SBE interface
+ * failure will be conveyed via respective exceptions.
+ *
+ * Exceptions thrown for:
+ * - SBE Internal failures
+ *
+ * @param[in] SBE data obtained from the SBE FIFO device
+ * @return Valid chip operation response obtained by SBE.
+ */
+std::vector<sbe_word_t> parseResponse(
+    const std::vector<sbe_word_t>& sbeDataBuf);
+
+}//end of fifoutils namespace
+
+/**
+ * @brief Interface to invoke a SBE chip operation.It calls internal API to
+ * write to the SBE FIFO and validates the data obtained by the SBE. It throws
+ * exception for any SBE internal failures.
+ *
+ * Runtime exceptions thrown for:
+ * - Device driver failures
+ * - SBE internal failures
+ *
+ * @param[in] FIFO device path associated with the SBE.
+ * @param[in] Request packet for the data to be read.
+ * @param[in] Data obtained by the SBE.
+ */
+template<size_t S1, size_t S2>
+void invokeSBEChipOperation(const char* devPath,
+                            const std::array<sbe_word_t, S1>& request,
+                            std::array<sbe_word_t, S2>& chipOpData)
+{
+    //Write and read from the FIFO device.
+    std::vector<sbe_word_t> sbeFifoResp;
+    sbeFifoResp = fifoutils::write(devPath, request.data(), request.size(),
+                                   chipOpData.size());
+
+    //Parse the obtained data
+    auto response = fifoutils::parseResponse(sbeFifoResp);
+
+    if (response.size() != chipOpData.size())
+    {
+        //TODO:use elog infrastructure
+        std::ostringstream errMsg;
+        errMsg << "Obtained chip operation response length (" << response.size()
+               << "from SBE is not equal to the expected length of data (" <<
+               chipOpData.size();
+
+        throw std::runtime_error(errMsg.str().c_str());
+    }
+
+    //Move the contents of response buffer into the output buffer.
+    std::move(response.begin(), response.end(), chipOpData.begin());
+}
+
+}
+}
+
+
