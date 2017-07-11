895f27aa1a8021bdcaeaeb2bea81901547c5a1ae
Murulidhar Nataraju <murulidhar@in.ibm.com>
2017-07-11 08:55:33 -0500

    Add calls to the SBE FIFO device driver to perform the SBE chip operations.
    Change-Id: I2f1204efc8196e9d331789e7910f90537834ddde
    Signed-off-by: Murulidhar Nataraju <murulidhar@in.ibm.com>

--- sbe_chipOp_handler.hpp
+++ sbe_chipOp_handler.hpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <algorithm>
 #include <vector>
+#include <sbe_interfaces.hpp>
 
 namespace openpower
 {
@@ -31,10 +32,10 @@ namespace internal
  *
  * @return Response buffer returned by the SBE for the input command.
  */
-std::vector<sbe_word_t> write(const char* devPath,
-                              const sbe_word_t* cmdBuffer,
-                              size_t cmdBufLen,
-                              size_t respBufLen);
+std::vector<sbe_word_t> writeToFifo(const char* devPath,
+                                    const sbe_word_t* cmdBuffer,
+                                    const size_t cmdBufLen,
+                                    const size_t respBufLen);
 
 /**
  * @brief Helper function for invokeSBEChipOperation(), to parse the data
@@ -74,8 +75,8 @@ inline void invokeSBEChipOperation(const char* devPath,
                                    std::array<sbe_word_t, S2>& chipOpData)
 {
     //Write and read from the FIFO device.
-    auto sbeFifoResp = internal::write(devPath, request.data(), request.size(),
-                                       chipOpData.size());
+    auto sbeFifoResp = internal::writeToFifo(devPath, request.data(),
+                       request.size(), chipOpData.size());
 
     //Parse the obtained data
     auto response = internal::parseResponse(sbeFifoResp);
