55c064362eb9a478ceeef3ffc39c99a2c36fa9d2
Murulidhar Nataraju <murulidhar@in.ibm.com>
2017-07-11 08:55:33 -0500

    Add calls to the SBE FIFO device driver to perform the SBE chip operations.
    Change-Id: I2f1204efc8196e9d331789e7910f90537834ddde
    Signed-off-by: Murulidhar Nataraju <murulidhar@in.ibm.com>

--- sbe_chipOp_handler.cpp
+++ sbe_chipOp_handler.cpp
@@ -1,5 +1,9 @@
-#include <stdexcept>
 #include <array>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <poll.h>
+#include <endian.h>
 #include <sbe_chipOp_handler.hpp>
 namespace openpower
 {
@@ -7,36 +11,116 @@ namespace sbe
 {
 namespace internal
 {
-/*! \union sbeRespHeader_t
- *  Defines the breakup of the SBE FIFO chip operation response header.
- */
-union sbeRespHeader_t
-{
-    uint32_t commandWord; /**<Entire 32 bit command word */
-    struct
-    {
-        uint8_t command: 8; /**<command value for which response is obtained */
-        uint8_t commandClass: 8; /**< Class of the command */
-        uint16_t magic: 16; /**< Magic code obtained in the response */
-    };
-} __attribute__((packed));
-
 
 constexpr uint16_t MAGIC_CODE = 0xC0DE;
 constexpr auto SBE_OPERATION_SUCCESSFUL = 0;
 constexpr auto LENGTH_OF_DISTANCE_HEADER_IN_WORDS = 0x1;
-constexpr auto SBEI_SBE_RESPONSE_SIZE_IN_WORDS = ( (sizeof(sbeRespHeader_t) + \
-                                                    sizeof(sbe_word_t)) / 4 );
+constexpr auto LENGTH_OF_RESP_HEADER_IN_WORDS = 0x2;
+constexpr auto MAX_FFDC_LEN_IN_WORDS = 5120;
 
-std::vector<sbe_word_t> write(const char* devPath,
-                              const sbe_word_t* cmdBuffer,
-                              size_t cmdBufLen,
-                              size_t respBufLen)
+std::vector<sbe_word_t> writeToFifo(const char* devPath,
+                                    const sbe_word_t* cmdBuffer,
+                                    const size_t cmdBufLen,
+                                    const size_t respBufLen)
 {
-    //TODO: Add support for reading and writing from the FIFO device
-    //Until then just return hardcoded data.
-    std::vector<sbe_word_t> response(respBufLen);
-    response = {0x80200000, 0x00000000, 0xc0dea201, 0x00000000, 0x00000003};
+    int fd = 0;
+    int rc = 0;
+    std::vector<sbe_word_t> response;
+    std::ostringstream errMsg;
+
+    do
+    {
+        //Open the FIFO Device
+        fd = open(devPath, O_RDWR | O_NONBLOCK);
+        if (fd < 0)
+        {
+            //TODO:use elog infrastructure
+            errMsg << "Opening SBE FIFO with device path:" <<
+                   devPath << ",Failed with errno" << errno;
+            break;
+        }
+
+        //Wait for FIFO device and perform write operation
+        struct pollfd pollfd;
+        pollfd.fd = fd;
+        pollfd.events = POLLOUT | POLLERR;
+
+        if ((rc = poll(&pollfd, 1, -1)) < 0)
+        {
+            //TODO:use elog infrastructure
+            errMsg << "Waiting for FIFO device:" << devPath << "to write failed"
+                   << "rc=" << rc << "errno=" << errno;
+            break;
+        }
+        if (pollfd.revents & POLLERR)
+        {
+            //TODO:use elog infrastructure
+            errMsg << "POLLERR while waiting for writeable FIFO";
+            break;
+        }
+        //Perform the write operation
+        rc = write(fd, cmdBuffer, (cmdBufLen * 4));
+        if (rc < 0)
+        {
+            //TODO:use elog infrastructure
+            errMsg << "Failed to write to FIFO device:" << devPath << " rc="
+                   << rc << " errno=" << errno;
+            break;
+        }
+
+        //Wait for FIFO device and perform read operation
+        pollfd.fd = fd;
+        pollfd.events = POLLIN | POLLERR;
+        if ((rc = poll(&pollfd, 1, -1) < 0))
+        {
+            //TODO:use elog infrastructure
+            errMsg << "Waiting for FIFO device:" << devPath << "to read failed"
+                   << " rc=" << rc << " and errno=" << errno;
+            break;
+        }
+        if (pollfd.revents & POLLERR)
+        {
+            //TODO:use elog infrastructure
+            errMsg << "POLLERR while waiting for readable FIFO";
+            break;
+        }
+
+        //Derive the total read length which should include the FFDC, which SBE
+        //returns in case of failure.
+        size_t totalReadLen = respBufLen + MAX_FFDC_LEN_IN_WORDS;
+        //Create a temporary buffer
+        std::vector<sbe_word_t> buffer(totalReadLen);
+
+        rc = read(fd, buffer.data(), (totalReadLen * 4));
+        if (rc < 0)
+        {
+            //TODO:use elog infrastructure
+            errMsg << "Failed to read the FIFO device:" << devPath << " rc="
+                   << rc << " errno=" << errno;
+            break;
+        }
+
+        //Extract the valid number of words read.
+        for (auto i = 0; i < (rc / 4); ++i)
+        {
+            response.push_back(be32toh(buffer[i]));
+        }
+
+    }
+    while (0);
+
+    //Close the file descriptor
+    if (fd >= 0)
+    {
+        close(fd);
+    }
+    //Check if we have any failure
+    if (errMsg.str().size())
+    {
+        //TODO:use elog infrastructure
+        throw std::runtime_error(errMsg.str().c_str());
+    }
+
     return (response);
 }
 
@@ -62,22 +146,21 @@ std::vector<sbe_word_t> parseResponse(
     }
 
     //Fetch the response header contents
-    sbeRespHeader_t l_respHeader={ };
     auto iter = sbeDataBuf.begin();
     std::advance(iter, (lengthObtained - distanceToStatusHeader));
-    l_respHeader.commandWord = *iter;
+    sbe_word_t l_magicCode = (*iter >> 16);
 
     //Fetch the primary and secondary response code
     std::advance(iter, 1);
     auto l_priSecResp = *iter;
 
     //Validate the magic code obtained in the response
-    if (l_respHeader.magic != MAGIC_CODE)
+    if (l_magicCode != MAGIC_CODE)
     {
         //TODO:use elog infrastructure
         std::ostringstream errMsg;
         errMsg << "Invalid MAGIC keyword in the response header (" <<
-               l_respHeader.magic << "),expected keyword " << MAGIC_CODE;
+               l_magicCode << "),expected keyword " << MAGIC_CODE;
         throw std::runtime_error(errMsg.str().c_str());
     }
 
@@ -86,11 +169,11 @@ std::vector<sbe_word_t> parseResponse(
     {
         //Extract the SBE FFDC and throw it to the caller
         size_t ffdcLen = (distanceToStatusHeader -
-                          SBEI_SBE_RESPONSE_SIZE_IN_WORDS -
+                          LENGTH_OF_RESP_HEADER_IN_WORDS -
                           LENGTH_OF_DISTANCE_HEADER_IN_WORDS);
 
         std::vector<sbe_word_t> ffdcData(ffdcLen);
-        std::copy_n((sbeDataBuf.begin() + SBEI_SBE_RESPONSE_SIZE_IN_WORDS),
+        std::copy_n((sbeDataBuf.begin() + LENGTH_OF_RESP_HEADER_IN_WORDS),
                     ffdcLen, ffdcData.begin());
 
         //TODO:use elog infrastructure to return the SBE and Hardware procedure
